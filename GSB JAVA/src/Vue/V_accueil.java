10dc29da3b26a43684cdbff83c03f138f0a43d4f
zoubert <zouberthanem@gmail.com>
2013-11-24 15:37:37 +0100

    Version 1.1 **Réctification des beugs et documentation de la fenêtre V_acceuil**

--- GSB JAVA/src/Vue/V_accueil.java
+++ GSB JAVA/src/Vue/V_accueil.java
@@ -28,15 +28,25 @@ public class V_accueil extends JFrame {
 	* */
 	public V_accueil () {
 		
-	    //TITRE DE LA FENETRE
+	    /*
+	     * TITRE DE LA FENETRE
+	     */
 		this.setTitle("Laboratoire Galaxy-Swiss Bourdin : Gestion des Fiches visiteurs");
-	    //TAILLE DE LA FENETRE
+	    /*
+	     * TAILLE DE LA FENETRE
+	     */
 	    this.setSize(800, 600);
-	    // LA FENETRE DOIT SE FERMER QUAND ON CLIQUE SUR LA CROIX ROUGE
+	    /*
+	     *  LA FENETRE DOIT SE FERMER QUAND ON CLIQUE SUR LA CROIX ROUGE
+	     */
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    //CENTRE LA FENETRE
+	    /*
+	     * CENTRE LA FENETRE
+	     */
 	    this.setLocationRelativeTo(null);
-	   
+	   /*
+	    * Empêche le redimensionement de la fenêtre
+	    */
 	    this.setResizable(false);
 
 	    /**
@@ -44,47 +54,70 @@ public class V_accueil extends JFrame {
 		 * @author Zoubert hanem
 		 */
 		this.icone = Toolkit.getDefaultToolkit().getImage("images/logo.png");
+		/*
+		 * Ajout de l'icone dans la fenêtre
+		 */
 		this.setIconImage(icone);
 		
+		/*
+		 * Couleur arriere-plan de la fenetre
+		 */
 		this.bgColor = Color.decode("#77aadd");
 		
-		//Panel choixVisiteur
+		/*
+		 * Panel choixVisiteur
+		 */
 		this.choixVis = new V_choixVisiteur();
 		
-		//Panel fiche frais
+		/*
+		 * Panel fiche frais
+		 */
 		this.fiche = new V_etatFrais();
 		this.fiche.setBackground(bgColor);
 		
-		//Panel accueil
+		/*
+		 * Panel de l'accueil
+		 */
 		this.panAccueil = new JPanel();
 		this.panAccueil.setBackground(bgColor);
 		
-		//MENU BAR
+		/*
+		 * MENU BAR
+		 */
 		this.menu = new JMenuBar();
 		
-		//MENU
+		/*
+		 * MENU
+		 */
 		this.consulter = new JMenu("Fiche");
 		this.validation = new JMenuItem("Validation");
 		this.suivi = new JMenuItem("Suivi");
 		
-		//ITEMS
+		/*
+		 * ITEMS
+		 */
 		this.deconnexion = new JMenuItem("Deconnexion");
 		this.FicheVisiteur = new JMenuItem("Consulter");
 		this.ToutesLesFiches = new JMenuItem("Toutes Les Fiches");
 		
-		//Label visiteur
-	
+		/*
+		 * Label visiteur
+		 */
 		this.nomVisiteur= new JLabel("<html>"+
 										"<h1 style=\"font-family:Comic Sans MS\">"+
 											"Visiteur : "+V_login.getJtfId().getText()+
 										"</h1>",JLabel.CENTER);
-		//AJOUT DE "JMenuBar->menu" DANS LA FENETRE
+		/*
+		 * AJOUT DE "JMenuBar->menu" DANS LA FENETRE
+		 */
 		this.setJMenuBar(menu);
 	
 		//AJOUT DU MENU "consulter" DANS le "JMenuBar->menu"
 		this.menu.add(consulter);
 		
-		//AJOUT DES ITEMS DANS LE MENU "consulter"
+		/*
+		 * AJOUT DES ITEMS DANS LE MENU "consulter"
+		 */
 		this.consulter.add(FicheVisiteur);
 		this.consulter.add(ToutesLesFiches);
 		this.consulter.add(validation);
@@ -92,13 +125,18 @@ public class V_accueil extends JFrame {
 		this.consulter.add(deconnexion);
 		
 
-		
+		/*
+		 * Ajout du label nomVisiteur dans le panel "paneAccueil"
+		 */
 		this.panAccueil.add(nomVisiteur);
 		
+		/*
+		 * Ajout du panel "panAccueil" dans la fenêtre
+		 */
 		this.getContentPane().add(panAccueil);
 		
 		/**
-		 * ACTION ITEM "FicheVisiteu"
+		 * ACTION ITEM "FicheVisiteur"
 		 * Cette action permet d'ouvrir le panel "ChoixVisiteur"
 		 * @author Zoubert hanem
 		 */
@@ -106,6 +144,9 @@ public class V_accueil extends JFrame {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				/*
+				 * Remplace le panel "panAccueil" par le panel "choixVis"
+				 */
 				setContentPane(choixVis);
 				setVisible(true);
 			}
